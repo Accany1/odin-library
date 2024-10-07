52838b2f7dc98928f48951e936439b3ad71d93c1
Accany1 <129660499+Accany1@users.noreply.github.com>
2024-10-07 11:35:53 +0800

    books have css and live in cards

--- javascript.js
+++ javascript.js
@@ -24,16 +24,50 @@ addBookToLibrary = (title, author, pages, read) => {
 
 displayBooks = (myLibrary) => {
     let titleList = []
+
+    const container = document.querySelector("#main")
+
     
+
     for (let item in myLibrary) {
         titleList.push(myLibrary[item].title)
-    }
 
-    return titleList
+        const card = document.createElement("div")
+        card.classList.add("card")
+        container.appendChild(card);
+
+        const title = document.createElement("div")
+        title.classList.add("title")
+        title.textContent = myLibrary[item].title
+        card.appendChild(title)
+
+        const author = document.createElement("div")
+        author.classList.add("author")
+        author.textContent = myLibrary[item].author
+        card.appendChild(author)
+
+        const pages = document.createElement("div")
+        pages.classList.add("pages")
+        pages.textContent = "Pages: " + myLibrary[item].pages
+        card.appendChild(pages)
+
+        const read = document.createElement("div")
+        read.classList.add("read")
+        if (myLibrary[item].read) {
+            read.textContent = "Read"
+        } else {
+            read.textContent = "Not read"
+        }
+        card.appendChild(read)
+    }
 }
 
-addBookToLibrary('aa', 'bb', '22', false)
-addBookToLibrary('cc', 'dd', '3213', true)
-addBookToLibrary('ee', 'ff', '1', false)
+addBookToLibrary('The Lord of the Rings', 'J. R. R. Tolkien', '1077', false)
+addBookToLibrary('1984', 'George Orwell', '328', true)
+addBookToLibrary('Kafka on the Shore', 'Haruki Murakami', '505', false)
 
-console.log(displayBooks(myLibrary))
+displayBooks(myLibrary)
+
+console.log('myLibrary')
+
+
